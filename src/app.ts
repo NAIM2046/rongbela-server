8787adcfb69ef6bb557d20609a6fd17714699ac7
jakir-md <mdjakir.cse.ju@gmail.com>
2026-07-07 19:58:17 +0600

    cors policy array added

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import globalErrorHandler from "./app/error/globalErrorHandler";
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://karutw.com", "https://www.karutw.com"],
     credentials: true,
     exposedHeaders: ["set-cookie"],
   }),
